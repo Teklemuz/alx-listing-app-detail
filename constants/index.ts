2c6bbded242cb6c58cbc9bbcc3d604aab927add8
Teklemuz Ayenew <111185891+Teklemuz@users.noreply.github.com>
2025-01-19 17:33:06 +0300

    Create index.ts

---
+++ constants/index.ts
@@ -0,0 +1,13 @@
+export const PROPERTYLISTINGSAMPLE = [
+  {
+    name: "Villa Ocean Breeze",
+    address: { state: "Seminyak", city: "Bali", country: "Indonesia" },
+    rating: 4.89,
+    category: ["Luxury Villa", "Pool", "Free Parking"],
+    price: 3200,
+    offers: { bed: "3", shower: "3", occupants: "4-6" },
+    image: "https://example.com/image1.jpg",
+    discount: "",
+    reviews: [],
+  },
+];
